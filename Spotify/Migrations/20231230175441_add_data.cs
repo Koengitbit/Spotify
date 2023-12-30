c7b5d61626ee24f83a8453ff0b9bdd3c536f6abd
Boas <boas181@hotmail.com>
2023-12-30 19:19:59 +0100

    Add new migration to insert data into Spotify Database
    In this commit, new migrations have been added, creating entities such as Albums, Artists, Playlists, and Songs, along with their relationships in the Spotify database. These migrations also handle data insertion and deletion in respective 'Up' and 'Down' methods.

---
+++ Spotify/Migrations/20231230175441_add_data.cs
@@ -0,0 +1,126 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Spotify.Migrations
+{
+    /// <inheritdoc />
+    public partial class adddata : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Title",
+                table: "Albums",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.InsertData(
+                table: "Albums",
+                columns: new[] { "Id", "Name", "Title" },
+                values: new object[,]
+                {
+                    { 1, "Album One", "Album One" },
+                    { 2, "Album Two", "Album Two" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Artists",
+                columns: new[] { "Id", "Name" },
+                values: new object[,]
+                {
+                    { 1, "Artist One" },
+                    { 2, "Artist Two" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Playlists",
+                columns: new[] { "Id", "IsPublic", "Name" },
+                values: new object[,]
+                {
+                    { 1, false, "Playlist One" },
+                    { 2, false, "Playlist Two" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Songs",
+                columns: new[] { "Id", "ArtistId", "Title" },
+                values: new object[,]
+                {
+                    { 1, 1, "Song One" },
+                    { 2, 2, "Song Two" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "PlaylistSong",
+                columns: new[] { "PlaylistId", "SongId" },
+                values: new object[,]
+                {
+                    { 1, 1 },
+                    { 2, 2 }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Albums",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Albums",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "PlaylistSong",
+                keyColumns: new[] { "PlaylistId", "SongId" },
+                keyValues: new object[] { 1, 1 });
+
+            migrationBuilder.DeleteData(
+                table: "PlaylistSong",
+                keyColumns: new[] { "PlaylistId", "SongId" },
+                keyValues: new object[] { 2, 2 });
+
+            migrationBuilder.DeleteData(
+                table: "Playlists",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Playlists",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Songs",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Songs",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Artists",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Artists",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DropColumn(
+                name: "Title",
+                table: "Albums");
+        }
+    }
+}
